a66bc4ed3112bd927cf1c7d9a095a600850f768b
jakobhertzberg <jakob.hertzberg@googlenmail.com>
2018-07-05 11:21:41 +0200

    added setup file

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+
+setup(name='string-kernel',
+      version='0.1',
+      description='Collection of string kernels',
+      long_description='This packages includes a variety of string kernel methods. Each method transform the list of input strings into a format that can be used by machine learning algorithm (e.g. SVM).',
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'License :: MIT License',
+          'Programming Language :: Python :: 3.6',
+          'Topic :: Text Processing :: Machine Learning',
+      ],
+      keywords='string kernel SVM machine learning',
+      url='https://github.com/jakobhertzberg/string-kernel',
+      author='Meng Zhang, Mitra Darvish, Jakob Hertzberg',
+      author_email='jakob.hertzberg@gmail.com',
+      license='MIT',
+      packages=find_packages(),
+      install_requires=[
+      ],
+      include_package_data=True,
+      zip_safe=False)
