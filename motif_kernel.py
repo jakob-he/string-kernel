4f2a08fd0a2802e7efb33887d0eb4da5acebb651
jakobhertzberg <jakob.hertzberg@googlenmail.com>
2018-07-06 17:12:43 +0200

    A
    Added basic strucutre for the motif kernel
    
    The idea is to pass the given motifs to the processMotifs function, which converts the list of strings to a trie object.
    For each sequence the number of contained motifs is then computed with DFS. The number of motifs in a sequence can be used to compute the similarity between sequences.

---
+++ motif_kernel.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+'''
+Implementation of the motif kernel.
+[Asa Ben-Hur and Douglas L. Brutlag. Remote homology detection: a motif based approach. Bioinformatics,
+19:26–33, 2003.]
+'''
+
+from lib import trie
+
+
+def processMotifs(motifs: [str]) -> trie:
+    """Constructs a trie based on the given list of motifs"""
+
+
+
+def computemotifKernel(trie,sequence):
+    """Applies motif kernel on a sequence"""
+
+
+def motifKernel(Motifs,sequences):
+    """Main function that executres the motif kernel on a set of sequences"""
