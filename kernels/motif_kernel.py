bff6eb14781221d58c6fa67c664cced0491c1d79
jakobhertzberg <jakob.hertzberg@googlenmail.com>
2018-07-15 17:19:57 +0200

    Added option to return similarity matrix

--- kernels/motif_kernel.py
+++ kernels/motif_kernel.py
@@ -5,11 +5,11 @@ Implementation of the motif kernel.
 19:26–33, 2003.]
 '''
 # own libraries
-from lib.trie import Trie
+from kernels.lib.trie import Trie
 
 # 3rd party libraries
 import numpy as np
-from scipy import sparse
+from scipy.sparse import csr_matrix
 
 
 def processMotifs(motifs: [str]) -> Trie:
@@ -25,7 +25,7 @@ def search_sequence(trie: Trie, sequence: str) -> np.array:
     return matching_motifs
 
 
-def motifKernel(motifs: [str], sequences: [str]) -> csr_matrix:
+def motifKernel(motifs: [str], sequences: [str], return_kernel_matrix = False) -> csr_matrix:
     """
     Main function that executes the motif kernel on a set of sequences. The idea is convert strings into numerical
     values which allow for the use of machine learning algorithms.
@@ -35,13 +35,20 @@ def motifKernel(motifs: [str], sequences: [str]) -> csr_matrix:
         motifs: A list of strings where each string is build out three possible components:
             1. Characters of a common alphabet (e.g. {A,G,C,T}) -> this alphabet should be the same for all _motifs
             2. The wildcard character "."
-            3. Subsequences that contain 2 ore more characters of the alphabet (e.g. [AG] or [CT])
+            3. Subsequences that contain 2 ore more characters of the alphabet (e.g. [AG] or [CT]). "^" indicates that every character
+               but those in the subsequence could be part of the motif.
         sequences: A list of strings from the same aphabet as the motifs.
     Returns:
         The function returns a sparse matrix object which can be used to compute similarities between the sequences.
     """
     motif_trie = processMotifs(motifs)
-    return sparse.csr_matrix([search_sequence(motif_trie, sequence) for sequence in sequences])
+    search_results = [search_sequence(motif_trie, sequence) for sequence in sequences]
+    if return_kernel_matrix:
+        kernel_matrix = csr_matrix(np.einsum('ij,kj->ik', search_results,search_results))
+        return kernel_matrix
+    else:
+        return csr_matrix(search_results)
+
 
 
 # Test
